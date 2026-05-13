bfc922bfd4f61efa7d8566a1f009dae702581f12
Kays0K <kayson.games.youtub@gmail.com>
2026-05-13 10:51:56 -0300

    Desafio 1 Pronto

--- src/paginas/Compras.jsx
+++ src/paginas/Compras.jsx
@@ -3,14 +3,11 @@ import { useEffect } from 'react';
 export default function Compras() {
     useEffect(() => {
         const fetchData = async () => {
-            try {
+            
                 const response = await fetch('http://localhost:3000/compras');
                 const data = await response.json();
-
-                console.log("Lista de Compras recebida:", data);
-            } catch (error) {
-                console.error("Erro ao conectar com a API:", error);
-            }
+                console.log("Compras:", data);
+            
         };
 
         fetchData();
