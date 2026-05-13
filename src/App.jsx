464e5c530edac256269d802da7b68f97e110cd2b
Kays0K <kayson.games.youtub@gmail.com>
2026-05-13 09:48:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './paginas/Login'
+import Cadastro from './paginas/Cadastro'
+import Usuarios from './paginas/Usuarios'
+
+
+function App() {
+  const [tela, setTela] = useState('login')
+
+  const trocarDeTela = (pagina) => {
+    setTela(pagina)
+  }
+
+  const renderizar = () => {
+    if (tela === 'login') {
+      return <Login/>
+    } else if (tela === 'cadastro') {
+      return <Cadastro/>
+    } else if (tela === 'usuarios') {
+      return <Usuarios/>
+    } else {
+      return <Login/>
+    }
+  }
+
+  return (
+    <>
+      <button onClick={() => trocarDeTela('cadastro')}>Cadastro</button>
+      <button onClick={() => trocarDeTela('login')}>Login</button>
+      <button onClick={() => trocarDeTela('usuarios')}>Usuarios</button>
+      
+
+      <hr />
+
+      {renderizar()}
+    </>
+  )
+}
+
+export default App
